ad8c8726328f5b9181ae52db531c433e9983de19
Matthew <31420840+MatthewGCampbell@users.noreply.github.com>
2021-11-07 10:21:40 -0500

    Make Bubbles Move

--- getthebiggestmovers.js
+++ getthebiggestmovers.js
@@ -5,6 +5,19 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// place bubbles in random places
+
+function random_bubble_pos() {
+  var x = Math.floor(Math.random() * (window.innerWidth - 50));
+  var y = Math.floor(Math.random() * (window.innerHeight - 50));
+  return [x, y];
+}
+function place_bubble(bubble) {
+  var pos = random_bubble_pos();
+  bubble.style.left = pos[0] + 'px';
+  bubble.style.top = pos[1] + 'px';
+}
+
 function waitForElement(){
   if(typeof coin1_info !== "undefined"){
     document.getElementById("coin1-info").innerText = capitalizeFirstLetter(coin1_info.item.id);
@@ -14,13 +27,20 @@ function waitForElement(){
     document.getElementById("coin5-info").innerText = capitalizeFirstLetter(coin5_info.item.id);
     document.getElementById("coin6-info").innerText = capitalizeFirstLetter(coin6_info.item.id);
     document.getElementById("coin7-info").innerText = capitalizeFirstLetter(coin7_info.item.id);
-    // CryptoLogos
-    document.createElement("img").src = coin1_info.item.thumb;
+    var bubbles = document.getElementsByClassName('bubbles');
+    for (var i = 0; i < bubbles.length; i++) {
+      place_bubble(bubbles[i]);
+    }
+    document.getElementsByClassName('bubbles')[0].style.visibility = 'visible';
   }
   else{
       setTimeout(waitForElement, 250);
   }
 }
+window.addEventListener('load', function () {
+  document.getElementsByClassName('bubbles')[0].style.visibility = 'hidden';
+})
+
 window.onload=function(){
     // Get Data From API On Button Click and Render
       function getData() {

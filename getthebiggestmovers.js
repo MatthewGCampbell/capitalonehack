02ff0e1c8c03322cf6225c6dc060c4f3271c94f5
Matthew <31420840+MatthewGCampbell@users.noreply.github.com>
2021-11-07 09:37:33 -0500

    Capitalize Names

--- getthebiggestmovers.js
+++ getthebiggestmovers.js
@@ -1,28 +1,24 @@
 // Get Data From Coin Gecko API (Trending Coins)
 // API: https://api.coingecko.com/api/v3/search/trending
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 function waitForElement(){
   if(typeof coin1_info !== "undefined"){
-    document.getElementById("coin1-info").innerText = coin1_info.item.id;
-    document.getElementById("coin2-info").innerText = coin2_info.item.id;
-    document.getElementById("coin3-info").innerText = coin3_info.item.id;
-    document.getElementById("coin4-info").innerText = coin4_info.item.id;
-    document.getElementById("coin5-info").innerText = coin5_info.item.id;
-    document.getElementById("coin6-info").innerText = coin6_info.item.id;
-    document.getElementById("coin7-info").innerText = coin7_info.item.id;
+    document.getElementById("coin1-info").innerText = capitalizeFirstLetter(coin2_info.item.id);
+    document.getElementById("coin2-info").innerText = capitalizeFirstLetter(coin2_info.item.id);
+    document.getElementById("coin3-info").innerText = capitalizeFirstLetter(coin3_info.item.id);
+    document.getElementById("coin4-info").innerText = capitalizeFirstLetter(coin4_info.item.id);
+    document.getElementById("coin5-info").innerText = capitalizeFirstLetter(coin5_info.item.id);
+    document.getElementById("coin6-info").innerText = capitalizeFirstLetter(coin6_info.item.id);
+    document.getElementById("coin7-info").innerText = capitalizeFirstLetter(coin7_info.item.id);
     // CryptoTickers
-    document.getElementById("coin1-ticker").innerText = coin1_info.item.symbol;
-    document.getElementById("coin2-ticker").innerText = coin2_info.item.symbol;
-    document.getElementById("coin3-ticker").innerText = coin3_info.item.symbol;
-    document.getElementById("coin4-ticker").innerText = coin4_info.item.symbol;
-    document.getElementById("coin5-ticker").innerText = coin5_info.item.symbol;
-    document.getElementById("coin6-ticker").innerText = coin6_info.item.symbol;
-    document.getElementById("coin7-ticker").innerText = coin7_info.item.symbol;
+    document.getElementById("coin1-info").innerText = coin1_info.item.symbol;
   }
   else{
       setTimeout(waitForElement, 250);
   }
 }
-
 window.onload=function(){
     // Get Data From API On Button Click and Render
       function getData() {
